24be7725fa64d113e722acadd4dbda90900b77c2
EDUARDO FARIA BRAGA <60194135004@senacrs.edu.br>
2025-10-16 21:02:07 -0300

    projeto final vercel

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'public'),
+  root: path.resolve(__dirname, 'src/public'), // aponta pro index.html
   build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, '/index.html') // entrada principal
-      }
-    },
-    outDir: path.resolve(__dirname, 'dist'), // pasta do build
+    outDir: path.resolve(__dirname, 'dist'),   // pasta final do build na raiz
     emptyOutDir: true
   }
 });
