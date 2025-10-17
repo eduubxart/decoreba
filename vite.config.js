bd00288d07a240ca58051af0c78b4c142dffb70d
EDUARDO FARIA BRAGA <60194135004@senacrs.edu.br>
2025-10-16 21:26:49 -0300

    atualizando

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src/public'), // aponta pro index.html
+  root: 'src/public',
   build: {
-    outDir: path.resolve(__dirname, 'dist'),   // pasta final do build na raiz
+    outDir: '../../dist',
     emptyOutDir: true
   }
 });
